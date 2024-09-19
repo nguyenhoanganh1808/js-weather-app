e1cd658baf16c30d9eedc2ed5817fbdb7261ad76
nguyenhoanganh1808 <1808nguyenhoanganh2003@gmail.com>
2024-09-19 10:08:54 +0700

    fetch weather when submit form

--- src/index.js
+++ src/index.js
@@ -1,74 +1,17 @@
 import './style.css';
+import { fetchWeather } from './weather';
 
-function weather(data) {
-  function getNextFiveHours() {
-    const date = new Date();
-    const currentHour = date.getHours();
+function screenController() {
+  const searchField = document.querySelector('#location');
+  const submitButton = document.querySelector('button');
 
-    return [...data.days[0].hours]
-      .filter((hourData) => {
-        const currentDateAndHour = date;
-        currentDateAndHour.setHours(hourData.datetime.split(':')[0]);
-
-        const hour = currentDateAndHour.getHours();
-
-        return hour > currentHour && hour <= currentHour + 5;
-      })
-      .map((hourData) => ({
-        temp: hourData.temp,
-        icon: hourData.icon,
-      }));
+  async function submitForm(e) {
+    e.preventDefault();
+    const weatherData = await fetchWeather(searchField.value);
+    console.log(weatherData);
   }
 
-  function getNextFiveDays() {
-    const currentDate = new Date();
-    return [...data.days]
-      .filter((dayData, index) => {
-        const dataDate = new Date(dayData.datetime);
-
-        const diffInTime = dataDate.getTime() - currentDate.getTime();
-        const diffInDays = diffInTime / (1000 * 3600 * 24);
-
-        return diffInDays > 0 && diffInDays <= 5;
-      })
-      .map((dayData) => ({
-        temp: dayData.temp,
-        icon: dayData.icon,
-        date: new Date(dayData.datetime),
-      }));
-  }
-  console.log(getNextFiveDays());
-  getNextFiveDays();
-  return {
-    address: data.resolvedAddress,
-    temperature: data.currentConditions.temp,
-    datetime: data.currentConditions.datetime,
-    humidity: data.currentConditions.humidity,
-    wind: data.currentConditions.windspeed,
-    fellslike: data.currentConditions.fell,
-    visibility: data.currentConditions.visibility,
-    sunrise: data.currentConditions.sunrise,
-    sunset: data.currentConditions.sunset,
-    tempmax: data.currentConditions.tempmax,
-    tempmin: data.currentConditions.tempmin,
-    hours: getNextFiveHours(),
-    days: getNextFiveDays(),
-  };
+  submitButton.addEventListener('click', submitForm);
 }
 
-async function fetWeather(location) {
-  const API_KEY = 'VDKUKL8EQDV9T899DT62TFX73';
-  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${API_KEY}`;
-
-  try {
-    const response = await fetch(url, { mode: 'cors' });
-    const data = await response.json();
-    console.log(data);
-    const hehe = weather(data);
-    console.log(hehe);
-  } catch (err) {
-    console.log(err);
-  }
-}
-
-fetWeather('Ho Chi Minh');
+screenController();
---
+++ src/weather.js
@@ -0,0 +1,71 @@
+function weather(data) {
+  function getNextFiveHours() {
+    const date = new Date();
+    const currentHour = date.getHours();
+
+    return [...data.days[0].hours]
+      .filter((hourData) => {
+        const currentDateAndHour = date;
+        currentDateAndHour.setHours(hourData.datetime.split(':')[0]);
+
+        const hour = currentDateAndHour.getHours();
+
+        return hour > currentHour && hour <= currentHour + 5;
+      })
+      .map((hourData) => ({
+        temp: hourData.temp,
+        icon: hourData.icon,
+      }));
+  }
+
+  function getNextFiveDays() {
+    const currentDate = new Date();
+    return [...data.days]
+      .filter((dayData) => {
+        const dataDate = new Date(dayData.datetime);
+
+        const diffInTime = dataDate.getTime() - currentDate.getTime();
+        const diffInDays = diffInTime / (1000 * 3600 * 24);
+
+        return diffInDays > 0 && diffInDays <= 5;
+      })
+      .map((dayData) => ({
+        temp: dayData.temp,
+        icon: dayData.icon,
+        date: new Date(dayData.datetime),
+      }));
+  }
+
+  return {
+    address: data.resolvedAddress,
+    temperature: data.currentConditions.temp,
+    datetime: data.currentConditions.datetime,
+    humidity: data.currentConditions.humidity,
+    wind: data.currentConditions.windspeed,
+    fellslike: data.currentConditions.fell,
+    visibility: data.currentConditions.visibility,
+    sunrise: data.currentConditions.sunrise,
+    sunset: data.currentConditions.sunset,
+    tempmax: data.currentConditions.tempmax,
+    tempmin: data.currentConditions.tempmin,
+    hours: getNextFiveHours(),
+    days: getNextFiveDays(),
+  };
+}
+
+async function fetchWeather(location) {
+  const API_KEY = 'VDKUKL8EQDV9T899DT62TFX73';
+  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${API_KEY}`;
+  let weatherData = {};
+
+  try {
+    const response = await fetch(url, { mode: 'cors' });
+    const data = await response.json();
+    weatherData = weather(data);
+  } catch (err) {
+    console.log(err);
+  }
+  return weatherData;
+}
+
+export { fetchWeather };
